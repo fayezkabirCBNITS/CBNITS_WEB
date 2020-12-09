36b30bc2c6e95f798e41c447039c893c8ec21148
Anirban <anirban.mandol@cbnits.com>
2020-12-09 17:06:59 +0530

    home page

--- app/components/SpecilalisedDomain/specialisec.js
+++ app/components/SpecilalisedDomain/specialisec.js
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from "react";
-import {
-  Nav,
-  NavItem,
-  Button,
-  NavLink,
-  Row,
-  Col,
-  Container,
-  TabContent,
-  TabPane,
-} from "reactstrap";
+import { Button, Row, Col, Container } from "reactstrap";
 import "./sepcialised.css";
-import health from "./../../images/health.png";
-import healthcare from "./../../images/h1.png";
-import security from "./../../images/h2.png";
-import EdTech from "./../../images/h3.png";
-import hrTech from "./../../images/h4.png";
-import eCommerce from "./../../images/h5.png";
-import classnames from "classnames";
-import axios from "axios";
 import { withRouter } from "react-router-dom";
 
 const a = ["healthcare", "security", "ED tech", "Hr tech", "E-commerce"];
 
 const Specialised = (props) => {
   const [activeTab, setActiveTab] = useState("1");
-  const [activeColor ,setActiveColor] = useState("");
-  const [readMoreLink , setReadmoreLink] = useState("");
-  const [specialDomain, setSpecialDomain] = useState([]);
+  const [activeColor, setActiveColor] = useState("");
+  const [readMoreLink, setReadmoreLink] = useState("");
   const [activeIndex, setActiveIndex] = useState(0);
   const [activeIndexData, setActiveIndexData] = useState([]);
+
+  const specialDomain = props.specialDomain;
+
+  useEffect(() => {
+    if (props.specialDomain && props.specialDomain.length) {
+      setActiveIndexData(props.specialDomain[0]);
+      setActiveColor(props.specialDomain[0].color);
+      setReadmoreLink(props.specialDomain[0].link);
+    }
+  }, [specialDomain]);
 
   const toggle = (tab) => {
     if (activeTab !== tab) {
@@ -38,37 +29,11 @@ const Specialised = (props) => {
   };
 
   useEffect(() => {
-    getSpecialDomain();
-  }, []);
-
-  useEffect(() => {
     setActiveIndexData(specialDomain[activeIndex]);
   }, [activeIndex]);
 
-  const getSpecialDomain = () => {
-    axios({
-      method: "POST",
-      url: "https://api.cbnits.com/getHomePageImagebyCategory",
-      data: {
-        category: "Special Domains",
-      },
-    })
-      .then((res) => {
-        if (res) {
-          setSpecialDomain(res.data.data);
-          console.log(res.data.data[0]);
-          setActiveIndexData(res.data.data[0]);
-          setActiveColor(res.data.data[0].color);
-          setReadmoreLink(res.data.data[0].link);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const gotoReadMore = _ => props.history.push(`/${readMoreLink}`);
+  const gotoReadMore = (_) => props.history.push(`/${readMoreLink}`);
 
-  console.log(activeColor , readMoreLink , "--------------------------")
   return (
     <div className="domain py-5">
       <Container className="themed-container" fluid={false}>
@@ -76,88 +41,42 @@ const Specialised = (props) => {
           <h2>
             our specialised<span> domains</span>
           </h2>
-          {/* <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
-            ipsum suspendisse ultrices gravida. Risus commas accumsan lacus vel
-            facilisis.{" "}
-          </p> */}
         </div>
         <Row className="d-flex align-items-center justify-content-between">
           <Col xl={6} lg={6} className="d-flex align-items-center leftTopic">
             <div className="topic">
-              <img src={activeIndexData?activeIndexData.thumbnailPic : ""} alt="health" />
+              <img
+                src={activeIndexData ? activeIndexData.thumbnailPic : ""}
+                alt="health"
+              />
             </div>
-            <h2>{activeIndexData?activeIndexData.name : ""}</h2>
+            <h2>{activeIndexData ? activeIndexData.name : ""}</h2>
           </Col>
           <Col xl={6} lg={6} className="desp-topic">
-            <p>{activeIndexData?activeIndexData.description : ""}</p>
-            <Button color="primary" onClick={gotoReadMore}>Read more</Button>
+            <p>{activeIndexData ? activeIndexData.description : ""}</p>
+            <Button color="primary" onClick={gotoReadMore}>
+              Read more
+            </Button>
           </Col>
         </Row>
 
-        {/* <div className="category-container">
-          <Nav tabs>
-            {a.map((data, index) => (
-              <NavItem key={index}>
-                <NavLink
-                  className={classnames({
-                    "green-back": activeTab === `${index + 1}`,
-                  })}
-                  onClick={() => {
-                    toggle(`${index + 1}`);
-                  }}
-                >
-                  <div className="text-center position-relative">
-                    <img src={healthcare} alt="" />
-                    <p>{data}</p>
-                  </div>
-                </NavLink>
-              </NavItem>
-            ))}
-          </Nav>
-        </div> */}
-
         <div className="category-container">
-          {specialDomain.map((data, index) =>  (
+          {specialDomain.map((data, index) => (
             <div
-              className={`text-center position-relative health-tab  ${activeColor === data.color ? `${activeColor}-active` : ""}`
-              // ${activeIndex === index && activeIndex == 0 ? "green-active" : ""}
-              // ${activeIndex === index && activeIndex == 1 ? "purple-active" : ""}
-              // ${activeIndex === index && activeIndex == 2 ? "orange-active" : ""}
-              // ${activeIndex === index && activeIndex == 3 ? "sky-active" : ""}
-              // ${activeIndex === index && activeIndex == 4 ? "blue-active" : ""}
-
-                      
-            }
+              className={`text-center position-relative health-tab  ${
+                activeColor === data.color ? `${activeColor}-active` : ""
+              }`}
               key={index}
-              onClick={() => {setActiveIndex(index) , setActiveColor(data.color) , setReadmoreLink(data.link)}}
+              onClick={() => {
+                setActiveIndex(index),
+                  setActiveColor(data.color),
+                  setReadmoreLink(data.link);
+              }}
             >
               <img src={data.logoPic} alt="" />
               <p>{data.name}</p>
             </div>
-          )
-          )}
-          {/* <div className="text-center position-relative green green-back">
-            <img src={specialDomain[0]} alt="" />
-            <p>healthcare</p>
-          </div>
-          <div className="text-center position-relative purple">
-            <img src={security} alt="" />
-            <p>security</p>
-          </div>
-          <div className="text-center position-relative orange">
-            <img src={EdTech} alt="" />
-            <p>ED tech</p>
-          </div>
-          <div className="text-center position-relative sky">
-            <img src={hrTech} alt="" />
-            <p>Hr tech</p>
-          </div>
-          <div className="text-center position-relative blue">
-            <img src={eCommerce} alt="" />
-            <p>E-commerce</p>
-          </div> */}
+          ))}
         </div>
       </Container>
     </div>
